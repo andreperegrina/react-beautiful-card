aafe5afbe325f5741dce53ce0f634259af86cfef
Andre Peregrina <andre.peregrina@gmail.com>
2020-10-27 00:38:29 -0700

    Fix: css layout

--- src/App.js
+++ src/App.js
@@ -6,54 +6,56 @@ import image from "./meeting.svg";
 function App() {
    return (
       <div className='App'>
-         <div className='title-container'>
-            <h1>Beautiful Cards, Amazing colors!</h1>
-         </div>
-         <div className='list-cards'>
-            <Card
-               size='md'
-               color='color-1'
-               onClick={() => {
-                  console.log('clicked')
-               }}
-               image={image}
-               title={'Build a web app with React Hooks'}
-               subtitle={'Early Access'}
-               buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
-            />
-            <Card
-               size='md'
-               color='color-2'
-               onClick={() => {
-                  console.log('clicked')
-               }}
-               image={image}
-               title={'Build a web app with React Hooks'}
-               subtitle={'Early Access'}
-               buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
-            />
-            <Card
-               size='md'
-               color='color-3'
-               onClick={() => {
-                  console.log('clicked')
-               }}
-               image={image}
-               title={'Build a web app with React Hooks'}
-               subtitle={'Early Access'}
-               buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
-            />
-            <Card
-               size='md'
-               color='color-4'
-               onClick={() => {
-                  console.log('clicked')
-               }}
-               image={image}
-               title={'Build a web app with React Hooks'}
-               subtitle={'Early Access'}
-               buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
-            />
+         <div className="container">
+            <div className='title-container'>
+               <h1>Beautiful Cards, Amazing colors!</h1>
+            </div>
+            <div className='list-cards'>
+               <Card
+                  size='md'
+                  color='color-1'
+                  onClick={() => {
+                     console.log('clicked')
+                  }}
+                  image={image}
+                  title={'Build a web app with React Hooks'}
+                  subtitle={'Early Access'}
+                  buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
+               />
+               <Card
+                  size='md'
+                  color='color-2'
+                  onClick={() => {
+                     console.log('clicked')
+                  }}
+                  image={image}
+                  title={'Build a web app with React Hooks'}
+                  subtitle={'Early Access'}
+                  buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
+               />
+               <Card
+                  size='md'
+                  color='color-3'
+                  onClick={() => {
+                     console.log('clicked')
+                  }}
+                  image={image}
+                  title={'Build a web app with React Hooks'}
+                  subtitle={'Early Access'}
+                  buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
+               />
+               <Card
+                  size='md'
+                  color='color-4'
+                  onClick={() => {
+                     console.log('clicked')
+                  }}
+                  image={image}
+                  title={'Build a web app with React Hooks'}
+                  subtitle={'Early Access'}
+                  buttons={[{key: 1, text: '👍'}, {key: 2, text: '👏'}]}
+               />
+            </div>
          </div>
       </div>
    );
